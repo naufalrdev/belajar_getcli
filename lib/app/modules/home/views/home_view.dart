a4af5e35e477c9ab827cfa3f076a732588a3cb1c
naufalrdev <naufalrifkidwiputra3081@gmail.com>
2025-11-15 20:48:32 +0700

    Initialized commit

---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,21 @@
+import 'package:belajar_getcli/app/routes/app_pages.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('HomeView'), centerTitle: true),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Get.toNamed(Routes.PRODUCTS),
+          child: Text("Go to Product Page"),
+        ),
+      ),
+    );
+  }
+}
